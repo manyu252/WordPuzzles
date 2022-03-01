04a5a184c11b4738691f73806b291c04c4fe60fb
manyu252 <abhimanyu@mandalore.local>
2022-03-01 15:37:14 +0530

    Added alphabets to include/omit in the output words Some changes in the way I am sending the output

--- find_words.py
+++ find_words.py
@@ -31,29 +31,67 @@ class GetWords(object):
         return self.words
 
     def check_validity(self):
-        result = list(set(self.words) & set(self.valid_words))
+        self.words = list(set(self.words) & set(self.valid_words))
         # print("words:", self.words)
-        return result
+        return
+
+    def print_words(self):
+        if len(self.words) == 0:
+            print("No such word exists. Please check your conditions and the incomplete word you entered")
+            return
+        for word in self.words:
+            print(word)
+        return
+
+    def include_words(self, include):
+        self.words = [word for word in self.words if any(char in word for char in include)]
+        return
+
+    def remove_words(self, remove):
+        self.words = [word for word in self.words if all(char not in word for char in remove)]
+        return
 
 def main():
 
     gw = GetWords()
     if sys.version_info.major == 3:
         hints = input("Enter the incomplete word: ")
+        print("Just press 'Enter' if you don't have any specific conditions. Conditions -")
+        include = input("include alphabets: ")
+        remove = input("omit alphabets: ")
     else:
         hints = raw_input("Enter the incomplete word: ")
+        print("Just press 'Enter' if you don't have any specific conditions. Conditions -")
+        include = raw_input("include alphabets: ")
+        remove = raw_input("omit alphabets: ")
+
+    t1 = time.time()
+    if len(include) > 0:
+        include = include.lower()
+    else:
+        include = None
+
+    if len(remove) > 0:
+        remove = remove.lower()
+    else:
+        remove = None
 
     hints = hints.lower()
     hints = list(hints)
-    t1 = time.time()
 
     words = []
     gw.letter_add(hints, words)
+    if not remove == None:
+        gw.remove_words(remove)
+    if not include == None:
+        gw.include_words(include)
+    gw.check_validity()
 
-    result = gw.check_validity()
+    print("\nresult:")
+    gw.print_words()
+
     t2 = time.time()
-
-    print("result:", result)
+    print('\n')
     print("time:", t2-t1, " seconds")
 
 if __name__ == "__main__":
